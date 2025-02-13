c4aa61ea6f2832a38c39da0f886496c19c8e7866
Ajay <ajkharatmol@gmail.com>
2025-02-13 13:43:03 +0530

    deploy-Project

--- Tours_and_Travels/Tours_and_Travels/settings.py
+++ Tours_and_Travels/Tours_and_Travels/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g&!5c*_6f27x^ndgvsq!qyq+$pu7+s=1s^d4l%m972efr(*jp*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
